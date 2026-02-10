861f1bbead6fb25b54d0f64077f432d854f50d19
https://github.com/ayushigihub007 <ayushisharmafzd1@gmail.com>
2026-02-10 22:42:43 +0000

    Fix middleware ellipsis issue

--- hrms/settings.py
+++ hrms/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -47,7 +46,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 
 CORS_ALLOW_ALL_ORIGINS = True
 
